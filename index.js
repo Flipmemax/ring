c3fe634f0e0148ffc7ce978140ae84fe5f00d554
Flipmemax <maxphoogland@gmail.com>
2021-04-29 15:27:28 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+const toTitleCase = require("./node_modules/to-title-case");
+
+const lower = "The Lord of The Rings: The Return of The King";
+const title = toTitleCase(lower);
+
+console.log(title);
